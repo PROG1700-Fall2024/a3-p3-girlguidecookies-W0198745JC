288bd454b079b8d3a7b4978fd91b9c21e26fd218
W0198745JC <w0198745@campus.nscc.ca>
2024-11-19 13:28:07 -0400

    main loop and functions started

--- A3-P3-GirlGuideCookies.py
+++ A3-P3-GirlGuideCookies.py
@@ -4,12 +4,26 @@
 #               those guides who go above and beyond in their sales efforts. The organizers
 #               want a program to print the guide list and their prizes.
 
-#Student #:     
-#Student Name:  
-
+#Student #:     w0198745
+#Student Name:  Jenille Cheney 
+def CalcAvgBoxSold(_boxesSold,_guideNames):
+    return(sum(_boxesSold))/(len(_guideNames))
+def printAvgBoxes(_boxesSold,_guideNames):
+    averageBoxes=CalcAvgBoxSold(_boxesSold,_guideNames)
+    print(f"The average number of boxes sold by each guide was {averageBoxes}")    
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
+    NumOfGuides=int(input("Enter the number of guides selling cookies: "))
 
+    guideNames=[]
+    boxesSold=[]
+    for count in range(NumOfGuides):
+        guide=input(f"Enter the name of guide #{count+1}")
+        guideNames.append(guide)
+        numOfBoxes=input(f"Enter the number of boxes sold by {guideNames[count]}")
+        boxesSold.append(numOfBoxes)
+
+    printAvgBoxes(boxesSold,guideNames)
 
 
 
